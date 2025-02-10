1ff3d042c7b43f3a764bbad74c137b4f9f93fc85
Veerla Tejesh <tejesh926@gmail.com>
2025-02-10 16:41:27 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.8 MB (100.00%)

--- 0020-valid-parentheses/solution.py
+++ 0020-valid-parentheses/solution.py
@@ -1,23 +1,17 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        s = list(s)
-        L = ['(','{','[']
-        R = [')','}',']']
-        ans = []
-        for i in range(len(s)):
-            if s[i] in L:
-                ans.append(s[i])
-            elif s[i] in R:
-                ind = R.index(s[i])
-                if len(ans)==0 or L[ind] != ans[-1]:
-                    return False
+        d = {')':'(', '}':'{',']':'['}
+        st = []
+        for i in s:
+            if i in ["(","{",'[']:
+                st.append(i)
+            else:
+                if st:
+                    if d[i] == st[-1]:
+                        st.pop()
+                    else:
+                        print("here")
+                        return False
                 else:
-                    ans.pop()
-        if len(ans)==0:
-            return True
-        else:
-            return False
-            
-
-
-
+                    return False
+        return len(st)==0
