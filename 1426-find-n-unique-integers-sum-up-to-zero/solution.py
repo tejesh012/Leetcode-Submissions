7a22a851c0ba9df09f21555781bd2c5c2c3945c2
Veerla Tejesh <tejesh926@gmail.com>
2025-09-09 15:46:33 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.7 MB (100.00%)

---
+++ 1426-find-n-unique-integers-sum-up-to-zero/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def sumZero(self, n: int) -> List[int]:
+        a = []
+    
+        for i in range(1,n//2+1):
+            a.append(i)
+            a.append(-i)
+        if n%2!=0:
+            a.append(0)
+        return (a)
