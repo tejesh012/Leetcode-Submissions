c905380bb74abb46f9294718c5d3aa349f918baa
Veerla Tejesh <tejesh926@gmail.com>
2025-10-31 07:31:49 +0000

    Sync LeetCode submission Runtime - 34 ms (81.34%), Memory - 26.8 MB (100.00%)

---
+++ 1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minNumberOperations(self, target: List[int]) -> int:
+        res = target[0]
+        prev = target[0]
+        for i in range(1,len(target)):
+            if prev < target[i]:
+                res += target[i] - prev
+            prev = target[i]
+        return res
