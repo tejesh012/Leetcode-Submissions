903807088f33e5f4527dc037f4b46d25a6fc1898
Veerla Tejesh <tejesh926@gmail.com>
2025-11-06 23:44:21 +0000

    Sync LeetCode submission Runtime - 15 ms (95.66%), Memory - 17.9 MB (100.00%)

--- 3610-find-x-sum-of-all-k-long-subarrays-i/solution.py
+++ 3610-find-x-sum-of-all-k-long-subarrays-i/solution.py
@@ -6,18 +6,11 @@ class Solution:
             d = {}
             if i+k-1 < len(nums):
                 for j in range(i,i+k):
-                    if nums[j] in d:
-                        d[nums[j]] +=1
-                    else:
-                        d[nums[j]] = 1
+                    d[nums[j]] = d.get(nums[j],0)+1
                 a = (sorted(d.items(),key =lambda item: (item[1] , item[0]), reverse = True))
                 s = 0
-                # print(nums[i:k])
-                # print(d)
-                # print(a)
                 for j in range(min(x,len(a))):
                     s+=a[j][0]*a[j][1]
-                    
                 ans.append(s)
             else:
                 break
