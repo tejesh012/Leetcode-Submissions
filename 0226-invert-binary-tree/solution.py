ba8a51033c8ffa73433913b51a008587f3dae4d4
Veerla Tejesh <tejesh926@gmail.com>
2026-01-10 19:06:32 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.3 MB (20.56%)

---
+++ 0226-invert-binary-tree/solution.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        
+        def ans(root):
+            
+            if root is None:
+                return None
+            x = root.left
+            root.left = root.right
+            root.right = x
+            
+            ans(root.left)
+            ans(root.right)
+            return root
+        root = ans(root)
+        return root
