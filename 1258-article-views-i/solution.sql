d11c5a512e15f5c1deea73262242d867f8bf1aa3
Veerla Tejesh <tejesh926@gmail.com>
2026-01-15 17:21:31 +0000

    Sync LeetCode submission Runtime - 389 ms (97.52%), Memory - 0.0B (100.00%)

--- 1258-article-views-i/solution.sql
+++ 1258-article-views-i/solution.sql
@@ -1,5 +1,4 @@
-# Write your MySQL query statement below
-Select Distinct viewer_id as id
-From Views
-Where author_id = viewer_id
-order by viewer_id
+SELECT Distinct author_id as id
+from Views
+where author_id = viewer_id
+order by author_id
