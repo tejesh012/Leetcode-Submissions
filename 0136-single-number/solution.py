44f81ae90683b58d689b57e643dcb433f74dcc75
Veerla Tejesh <tejesh926@gmail.com>
2026-05-03 15:29:52 +0000

    Sync LeetCode submission Runtime - 7 ms (29.93%), Memory - 21.6 MB (10.46%)

--- 0136-single-number/solution.py
+++ 0136-single-number/solution.py
@@ -1,6 +1,11 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        ans = 0 
+        d = {}
         for i in nums:
-            ans = ans^i
-        return ans
+            if i in d:
+                d[i]+=1
+            else:
+                d[i] = 1
+        for i in d:
+            if d[i] ==1:
+                return i
