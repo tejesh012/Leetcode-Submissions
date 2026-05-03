5204b8442988101d10bc2de339c617892dc3f9b2
Veerla Tejesh <tejesh926@gmail.com>
2026-05-03 15:54:32 +0000

    Sync LeetCode submission Runtime - 15 ms (19.00%), Memory - 21.2 MB (49.69%)

--- 0169-majority-element/solution.py
+++ 0169-majority-element/solution.py
@@ -1,20 +1,12 @@
-# import statistics
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        target = len(nums)//2
-        val = 0
-        d= {}
+        target = (len(nums)//2)
+        d = {}
         for i in nums:
             if i in d:
-                d[i] += 1
-                if target <=d[i]:
-                    val = i
-                    target = d[i]
+                d[i]+=1
             else:
                 d[i] = 1
-                if target <=d[i]:
-                    val = i
-                    target = d[i]
-                
-        return val
-        
+        for i in d:
+            if d[i] > target:
+                return i
