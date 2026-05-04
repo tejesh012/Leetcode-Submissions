d26ef1989e9e3ccf5e5f41e1b6081ab4b49c8664
Veerla Tejesh <tejesh926@gmail.com>
2026-05-04 09:57:45 +0000

    Sync LeetCode submission Runtime - 3 ms (3.78%), Memory - 19.4 MB (28.22%)

--- 0048-rotate-image/solution.py
+++ 0048-rotate-image/solution.py
@@ -3,10 +3,21 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        for i in range(len(matrix)):
-            for j in range(i+1,len(matrix)):
-                matrix[i][j],matrix[j][i] = matrix[j][i],matrix[i][j]
-        # print(matrix)
-        for i in range(len(matrix)):
+        n = len(matrix)
+        for i in range(n):
+            for j in range(i+1,n):
+                print(i,j,matrix[i][j],matrix[j][i])
+                matrix[i][j] , matrix[j][i] = matrix[j][i] , matrix[i][j]
+        
+        for i in range(n):
             matrix[i] = matrix[i][::-1]
         
+    
+
+        # 00  01  02  03
+
+        # 10  11  12  13
+
+        # 20  21  22  23
+
+        # 30  31  32  33
