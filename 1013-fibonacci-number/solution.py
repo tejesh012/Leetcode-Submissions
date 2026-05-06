0d62a664e3d695c589819267359f9bfee61a2bba
Veerla Tejesh <tejesh926@gmail.com>
2026-05-06 09:54:15 +0000

    Sync LeetCode submission Runtime - 335 ms (8.63%), Memory - 19.1 MB (56.88%)

--- 1013-fibonacci-number/solution.py
+++ 1013-fibonacci-number/solution.py
@@ -1,15 +1,23 @@
 class Solution:
     def fib(self, n: int) -> int:
-        a = 0
-        b = 1
-        total = 1
-        if n ==0:
-            return 0
-        while n>1:
-            temp = b
-            b = a+b
-            a = temp
-            n-=1
-        return b
+        if n== 0 or n== 1:
+            return n
+        
+        return self.fib(n-1) + self.fib(n-2)
 
-# 0 1 1 2 3 5
+
+
+
+#         a = 0
+#         b = 1
+#         total = 1
+#         if n ==0:
+#             return 0
+#         while n>1:
+#             temp = b
+#             b = a+b
+#             a = temp
+#             n-=1
+#         return b
+
+# # 0 1 1 2 3 5
