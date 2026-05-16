84eaeb8c557a123275419c4db38d27ce857d3edf
Veerla Tejesh <tejesh926@gmail.com>
2026-05-16 04:54:48 +0000

    Sync LeetCode submission Runtime - 13 ms (30.61%), Memory - 20.9 MB (97.97%)

--- 0042-trapping-rain-water/solution.py
+++ 0042-trapping-rain-water/solution.py
@@ -1,25 +1,27 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
+
+        a = 0
         n = len(height)
         lmax = []
-        rmax =[0]*n
+        rmax = [0]*n
         prev = float("-inf")
         for i in range(n):
-            if height[i]>=prev:
+            if height[i] > prev:
                 lmax.append(height[i])
                 prev = height[i]
             else:
                 lmax.append(prev)
+        prev = float("-inf")
         
-
-        prev = float('-inf')
         for i in range(n-1,-1,-1):
-            if prev < height[i]:
-                rmax[i] = height[i]
+            
+            if height[i] > prev:
+                rmax[i]=height[i]
                 prev = height[i]
             else:
-                rmax[i] = prev
+                rmax[i]=prev
         res = 0
         for i in range(n):
-            res += max( 0, min(lmax[i],rmax[i])-height[i])
+            res+= min(lmax[i],rmax[i])-height[i]
         return res
