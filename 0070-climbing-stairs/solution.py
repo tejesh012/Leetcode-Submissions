6ebde126ecff78fb0885bf145173fe1b566ece91
Veerla Tejesh <tejesh926@gmail.com>
2026-05-17 05:01:53 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.3 MB (16.50%)

--- 0070-climbing-stairs/solution.py
+++ 0070-climbing-stairs/solution.py
@@ -2,14 +2,12 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         from functools import lru_cache
         @lru_cache
-        def solve(rem):
-            if rem == 0:
+        def solve(idx):
+            if idx == n:
                 return 1
-            if rem <0:
+            if idx>n:
                 return 0
-            twostep = 0
-            onestep = solve(rem-1)
-            twostep = solve(rem-2)
-            return onestep+twostep
-        
-        return solve(n)
+            take = solve(idx+1)
+            take2 = solve(idx+2)
+            return take+take2
+        return solve(0)
