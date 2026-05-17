ea583e0fd43a8a8825df20796830cd32fd3a454e
Veerla Tejesh <tejesh926@gmail.com>
2026-05-17 06:15:00 +0000

    Sync LeetCode submission Runtime - 3 ms (20.11%), Memory - 24.7 MB (5.05%)

---
+++ 0062-unique-paths/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        
+        dp =[[-1]*101 for _ in range(101)]
+        def solve(i,j):
+            if i>=m or j>=n:
+                return 0
+            if dp[i][j] !=-1:
+                return dp[i][j]
+            if i == m-1 and j == n-1:
+                return 1
+            dp[i][j] = solve(i+1,j)+solve(i,j+1)
+            return dp[i][j]
+        return solve(0,0)
+            
