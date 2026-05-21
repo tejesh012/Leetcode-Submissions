341b319b9d14b8fc42f865a71a4ce2e8173e9986
Veerla Tejesh <tejesh926@gmail.com>
2026-05-21 09:48:45 +0000

    Sync LeetCode submission Runtime - 19 ms (6.17%), Memory - 19.8 MB (39.13%)

--- 0125-valid-palindrome/solution.py
+++ 0125-valid-palindrome/solution.py
@@ -1,18 +1,18 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         n = len(s)
-        s = s.lower()
         p1 = 0
         p2 = n-1
-        while (p1<n and p2>=0):
-            if not s[p1].isalnum():
-                p1+=1
-                continue
-            elif not s[p2].isalnum():
-                p2-=1
-                continue
-            if s[p1]!=s[p2]:
+        
+        while p1 <= p2:
+            if s[p1] != " " and s[p2] != " " and s[p1].lower()!= s[p2].lower() and s[p1].isalnum() and s[p2].isalnum():
                 return False
-            p1 +=1
-            p2 -=1
+            if s[p1] == " " or not s[p1].isalnum():
+                p1+=1
+            elif s[p2] == " " or not s[p2].isalnum():
+                p2-=1
+            elif s[p1].lower() == s[p2].lower():
+                p1+=1
+                p2-=1
+            
         return True
