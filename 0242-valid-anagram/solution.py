9c4f4980a30dca7c14e6d50b12942a33f0418d4d
Veerla Tejesh <tejesh926@gmail.com>
2026-05-21 09:08:57 +0000

    Sync LeetCode submission Runtime - 16 ms (24.83%), Memory - 20.7 MB (9.87%)

--- 0242-valid-anagram/solution.py
+++ 0242-valid-anagram/solution.py
@@ -1,5 +1,3 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s = sorted(list(s))
-        t = sorted(list(t))
-        return s==t
+        return sorted(list(s)) == sorted(list(t))
