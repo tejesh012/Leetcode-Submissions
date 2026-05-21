3db5e1567f015bdc39da953328430ca99254047f
Veerla Tejesh <tejesh926@gmail.com>
2026-05-21 09:28:54 +0000

    Sync LeetCode submission Runtime - 11 ms (83.11%), Memory - 22.2 MB (53.71%)

--- 0049-group-anagrams/solution.py
+++ 0049-group-anagrams/solution.py
@@ -1,19 +1,8 @@
 class Solution:
+    from collections import defaultdict
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        d = {}
-        for i in range(len(strs)):
-            # print(strs[i])
-            x = "".join(sorted(strs[i]))
-            # print(x)
-            if x in d:
-                d[x].append(i)
-            else:
-                d[x]=[i]
-        
-        ans = []
-        for i in d:
-            med = []
-            for j in d[i]:
-                med.append(strs[j])
-            ans.append(med)
-        return ans
+        d = defaultdict(list)
+        for i in strs:
+            d["".join(sorted(i))].append(i)
+        return list(d.values())
+
