b54c3259a0376ccdecc5930adcce6f67e4cb5d99
Veerla Tejesh <tejesh926@gmail.com>
2026-06-11 20:11:32 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.3 MB (17.24%)

--- 3019-furthest-point-from-origin/solution.py
+++ 3019-furthest-point-from-origin/solution.py
@@ -1,3 +1,3 @@
 class Solution:
     def furthestDistanceFromOrigin(self, moves: str) -> int:
-        return max(moves.count("L"), moves.count("R"))+moves.count("_") - min(moves.count("L"), moves.count("R"))
+        return abs(moves.count("L") - moves.count("R"))+moves.count("_")
